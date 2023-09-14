9bd037c2359f72880771ea5c36c84ed3b9e5fd4c
Franck Lavisse <lavisse.franck@gmail.com>
2023-09-14 15:21:46 +0800

    concurrent

--- app/server.go
+++ app/server.go
@@ -14,26 +14,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
-	defer conn.Close()
+	done := make(chan bool, 1)
+	for i := 0; i < 10; i++ {
+		go func(done chan bool) {
+			conn, err := l.Accept()
+			defer conn.Close()
 
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+			for {
+				if err != nil {
+					fmt.Println("Error accepting connection: ", err.Error())
+					os.Exit(1)
+				}
+				var b = make([]byte, 1000)
+				_, err = conn.Read(b)
+				if err != nil {
+					fmt.Println("Failed to read")
+				}
+
+				_, err = conn.Write([]byte("+PONG\r\n"))
+				if err != nil {
+					fmt.Println("Failed to write")
+				}
+			}
+		}(done)
 	}
 
-	for {
-		var b = make([]byte, 1000)
-		_, err = conn.Read(b)
-		if err != nil {
-			fmt.Println("Failed to read")
-			os.Exit(1)
-		}
-
-		_, err = conn.Write([]byte("+PONG\r\n"))
-		if err != nil {
-			fmt.Println("Failed to write")
-			os.Exit(1)
-		}
-	}
+	<-done
 }
