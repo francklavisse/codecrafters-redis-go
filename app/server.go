7e79a300490f96f36c3cccfd9d8716ae51b394be
Franck Lavisse <lavisse.franck@gmail.com>
2023-09-18 10:47:21 +0800

    expiry

--- app/server.go
+++ app/server.go
@@ -37,8 +37,9 @@ func getResponse(cmd []string) string {
 		return "+" + cmd[4] + "\r\n"
 	case "SET", "set":
 		d := Data{Value: cmd[6], CreatedAt: time.Now(), PX: -1}
+		fmt.Println(cmd)
 		fmt.Println(len(cmd))
-		if len(cmd) > 6 && cmd[8] == "px" || cmd[8] == "PX" {
+		if cmd[8] == "px" || cmd[8] == "PX" {
 			px, err := strconv.Atoi(cmd[10])
 			if err == nil {
 				d.PX = px
